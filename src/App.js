019e427f5632cf42a6c4832dd3cacafa6af4b472
sunny06a <sunny.tomar.ug20@nsut.ac.in>
2023-06-12 20:37:04 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,13 +3,42 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './component/Navbar';
 import News from './component/News';
+import{
+  BrowserRouter ,
+  Route,
+  Routes
+} from "react-router-dom"
 
 export default class App extends Component {
   render() {
     return (
       <div>
+      <BrowserRouter>
         <Navbar></Navbar>
-        <News pagesize="5"></News>
+      <Routes>
+        <Route exact path='/' element={<News key="general" pagesize="5" country="in" category="general"></News>}></Route>
+      </Routes>
+      <Routes>
+        <Route exact path='/business' element={<News key="business"pagesize="5" country="in" category="business"></News>}></Route>
+      </Routes>
+      <Routes>
+        <Route exact path='/sports' element={<News key="sports" pagesize="5" country="in" category="sports"></News>}></Route>
+      </Routes>
+      <Routes>
+        <Route exact path='/technology' element={<News key="technology" pagesize="5" country="in" category="technology"></News>}></Route>
+      </Routes>
+      <Routes>
+        <Route exact path='/enterainment' element={<News key="entertainment" pagesize="5" country="in" category="entertainment"></News>}></Route>
+      </Routes>
+      <Routes>
+        <Route exact path='/science' element={<News key="science" pagesize="5" country="in" category="science"></News>}></Route>
+      </Routes>
+      <Routes>
+        <Route exact path='/health' element={<News key="health" pagesize="5" country="in" category="health"></News>}></Route>
+      </Routes>
+
+      </BrowserRouter>
+        
       </div>
     )
   }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,6 +1,6 @@
 //import PropTypes from "prop-types";
 import React, { Component } from "react";
-
+import {link} from "react-router-dom"
 export class Navbar extends Component {
   //static propTypes = {};
 
@@ -8,9 +8,9 @@ export class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <link className="navbar-brand" to="/">
             Daily News
-          </a>
+          </link>
           <button
             className="navbar-toggler"
             type="button"
@@ -25,15 +25,17 @@ export class Navbar extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <link className="nav-link" aria-current="page" to="/">
                   Home
-                </a>
+                </link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Link
-                </a>
-              </li>
+              <li className="nav-item"><link className="nav-link" to="/business">Business</link></li>
+              <li className="nav-item"><link className="nav-link" to="/entertainment">Entertainment</link></li>
+              <li className="nav-item"><link className="nav-link" to="/general">General</link></li>
+              <li className="nav-item"><link className="nav-link" to="/health">Health</link></li>
+              <li className="nav-item"><link className="nav-link" to="/sciencce">Science</link></li>
+              <li className="nav-item"><link className="nav-link" to="/sports">Sports</link></li>
+              <li className="nav-item"><link className="nav-link" to="/techonology">Technology</link></li>
             </ul>
           </div>
         </div>
